c4921203db5af077020ca174f91ff4a4c738a759
Jungmin-Noh <lumill925@gmail.com>
2022-08-19 13:52:39 +0900

    create new project

---
+++ src/main/java/com/sparta/cloneteam2backend/CloneTeam2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cloneteam2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneTeam2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneTeam2BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/cloneteam2backend/CloneTeam2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.cloneteam2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneTeam2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
